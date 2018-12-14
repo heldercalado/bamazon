ab12e6bf268fb582112f39860cb3676f0336a76e
Helder Calado <hbcalado@gmail.com>
2018-12-14 06:16:07 -0800

    maintenance I

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,20 +1,3 @@
 
-var dotEnv = require('dotenv').config();
-var Database = require('./db');
-var mysql      = require('mysql');
 
-db = new Database(); 
-
-
-var myObj = {
-    product_name:"Alexa Echo" ,
-    department_name:"Electronics" ,
-    price:12.50,
-    stock_quantity:20
-}
-
-
-//db.checkIfItemExists(myObj);
-
-//db.updateItemQuantity("Alexa Echo" , 10);
 
